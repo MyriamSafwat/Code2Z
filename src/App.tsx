e9dbddea17690d4da6a544d2508be45553d04efc
Nada El-Sayed  Abd El-Hamid <nosha2563@gmail.com>
2025-10-29 22:40:24 +0300

    Refactor App component to use Intro and CustomCursor
    Replaced Home component with Intro and CustomCursor components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import Home from "./pages/Home";
-export default function App() {
-    return (
-        <div>
-            <Home />
-            <h1 style={{ textAlign: "center" }}>The structure of the project is here</h1>
-        </div>
-    );
+import Intro from "./components/Intro";
+import CustomCursor from "./components/CustomCursor";
+import "./index.css";
+
+function App() {
+  return (
+    <div>
+      <Intro />
+      <CustomCursor />
+    </div>
+  );
 }
+
+export default App;
