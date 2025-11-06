17d2b390f3eb90fb098f4b6b9bf54d0b1e3f088b
lavinia <laviniaalfons@gmail.com>
2025-11-06 10:27:37 +0200

    double rendring fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import Intro from "./components/Intro";
 import CustomCursor from "./components/CustomCursor";
 import "./index.css";
-import { useState } from "react";
-import Home from "./pages/Home";
 
 function App() {
-  const [showHome, setShowHome] = useState(false);
   return (
     <div>
-      <Intro onFinish={() => setShowHome(true)} />
-      {showHome && <Home />}
+      <Intro />
       <CustomCursor />
     </div>
   );
